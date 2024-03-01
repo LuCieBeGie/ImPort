8b5fdd642812bff536ef63a35c557841fc9d1bfd
LuCieBeGie <lusineh.begoyan@gmail.com>
2024-03-01 11:47:50 +0100

    update Projects.css

--- my_portfolio/src/pages/Projects/Projects.js
+++ my_portfolio/src/pages/Projects/Projects.js
@@ -11,7 +11,7 @@ function Projects() {
             className="project_background projects"
             name="projects">
             <h1>My Projects</h1>
-            <figure className="">
+            <figure className="project_container">
                 {myProjects.map((project, projectId) => {
                     return <div key={projectId} className="division" >
                         <span className="image_wrapper">
@@ -21,7 +21,7 @@ function Projects() {
                         </span>
                         <figcaption>
                             <h2>{project.name}</h2>
-                            <span>{project.description}</span>
+                            <p style={{fontSize:'large'}}>{project.description}</p>
                             <br />
                             <a href={project.url} target="_blank" className="gitLink">GitHub 👉</a>
                         </figcaption>
